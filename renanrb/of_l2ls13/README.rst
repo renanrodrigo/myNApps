7c619bc7055309ef53a48ac9a5aa1a6d499ee06c
Renan Rodrigo <renanrb@ime.usp.br>
2017-09-21 15:30:22 -0300

    Add first version of the OpenFlow 1.3 l2ls

---
+++ renanrb/of_l2ls13/README.rst
@@ -0,0 +1,6 @@
+Overview
+========
+l2 learning switch using python-openflow version 1.3
+
+Requirements
+============
---
+++ renanrb/of_l2ls13/settings.py
@@ -0,0 +1,5 @@
+"""Settings for the of_l2ls NApp."""
+# LLDP Mac Addresses
+lldp_macs = ['01:80:c2:00:00:0e', '01:80:c2:00:00:03',
+             '01:80:c2:00:00:00']
+flow_priority = 10
